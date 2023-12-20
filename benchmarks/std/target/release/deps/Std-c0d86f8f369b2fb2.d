4333f2e788ec0bdf89f98154b1a5108a7db49e95
Martin Kayondo <kayondo@snu.ac.kr>
2023-12-20 23:04:43 +0900

    modifying SVF + Rust: adding MetaSafe + TRust

--- benchmarks/std/target/release/deps/Std-c0d86f8f369b2fb2.d
+++
@@ -1,7 +0,0 @@
-/home/iybang/project/benchmarks/std/target/release/deps/Std-c0d86f8f369b2fb2.bc: src/main.rs
-
-/home/iybang/project/benchmarks/std/target/release/deps/Std-c0d86f8f369b2fb2: src/main.rs
-
-/home/iybang/project/benchmarks/std/target/release/deps/Std-c0d86f8f369b2fb2.d: src/main.rs
-
-src/main.rs:
