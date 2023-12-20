4862adda3aa4884734740bf0af14ca3cc12babeb
Martin Kayondo <kayondomartin45@gmail.com>
2023-12-20 15:35:13 +0900

    fixes

--- mpk-rust-demangle/target/debug/deps/mpk_rust_demangle-fd9895a1e8f99e41.d
+++
@@ -1,5 +0,0 @@
-/home/martin/projects/metasafe/trust/mpk-rust-demangle/target/debug/deps/mpk_rust_demangle-fd9895a1e8f99e41.rmeta: src/lib.rs
-
-/home/martin/projects/metasafe/trust/mpk-rust-demangle/target/debug/deps/mpk_rust_demangle-fd9895a1e8f99e41.d: src/lib.rs
-
-src/lib.rs:
