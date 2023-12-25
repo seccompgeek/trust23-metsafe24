4c016efae8acb10530322312859fb12752277eea
martin <kayondo@snu.ac.kr>
2023-12-26 01:14:19 +0900

    remove garbage

--- benchmarks/std/target/x86_64-unknown-linux-gnu/release/deps/cfg_if-07546d78edb32999.d
+++
@@ -1,9 +0,0 @@
-/home/martin/projects/metasafe/trust-sec23/benchmarks/std/target/x86_64-unknown-linux-gnu/release/deps/cfg_if-07546d78edb32999.ll: /home/martin/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/martin/projects/metasafe/trust-sec23/benchmarks/std/target/x86_64-unknown-linux-gnu/release/deps/cfg_if-07546d78edb32999.rmeta: /home/martin/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/martin/projects/metasafe/trust-sec23/benchmarks/std/target/x86_64-unknown-linux-gnu/release/deps/libcfg_if-07546d78edb32999.rlib: /home/martin/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/martin/projects/metasafe/trust-sec23/benchmarks/std/target/x86_64-unknown-linux-gnu/release/deps/cfg_if-07546d78edb32999.d: /home/martin/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/martin/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
